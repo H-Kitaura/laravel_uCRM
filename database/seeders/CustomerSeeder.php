313f73e58fb4cb17651c93cd05cd2d6ba90855d7
HiroyukiKitaura <mr.worldwide_1212@icloud.com>
2022-12-05 22:16:53 +0900

    customers

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            UserSeeder::class,
+            ItemSeeder::class
+        ]);
+        \App\Models\Customer::factory(1000)->create();
+    }
+}
